8d03d5a5d1730ad2241c5da08cb0b9725f5d592e
jamieteegan <27767616+jamieteegan@users.noreply.github.com>
2022-09-26 12:08:54 +1000

    Update test_CVE_2007_4559.py
    Adding other CWE-022 vulnerability to test python CodeQL

--- test_CVE_2007_4559.py
+++ test_CVE_2007_4559.py
@@ -1,7 +1,45 @@
 # faulty code from CodeQL bad code sample
-import tarfile
+import tarfile, os.path
 
 with tarfile.open('archive.zip') as tar:
     #BAD : This could write any file on the filesystem.
     for entry in tar:
         tar.extract(entry, "/tmp/unpack/")
+
+        
+
+
+
+urlpatterns = [
+    # Route to user_picture
+    url(r'^user-pic1$', user_picture1, name='user-picture1'),
+    url(r'^user-pic2$', user_picture2, name='user-picture2'),
+    url(r'^user-pic3$', user_picture3, name='user-picture3')
+]
+
+
+def user_picture1(request):
+    """A view that is vulnerable to malicious file access."""
+    filename = request.GET.get('p')
+    # BAD: This could read any file on the file system
+    data = open(filename, 'rb').read()
+    return HttpResponse(data)
+
+def user_picture2(request):
+    """A view that is vulnerable to malicious file access."""
+    base_path = '/server/static/images'
+    filename = request.GET.get('p')
+    # BAD: This could still read any file on the file system
+    data = open(os.path.join(base_path, filename), 'rb').read()
+    return HttpResponse(data)
+
+def user_picture3(request):
+    """A view that is not vulnerable to malicious file access."""
+    base_path = '/server/static/images'
+    filename = request.GET.get('p')
+    #GOOD -- Verify with normalised version of path
+    fullpath = os.path.normpath(os.path.join(base_path, filename))
+    if not fullpath.startswith(base_path):
+        raise SecurityException()
+    data = open(fullpath, 'rb').read()
+    return HttpResponse(data)
